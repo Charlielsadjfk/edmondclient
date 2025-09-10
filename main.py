c759de04078d8e33926d4b363d34e09cfc3d1daf
Charlielsadjfk <charlie.shao@education.nsw.gov.au>
2025-09-10 14:08:52 +1000

    profile page not styled

--- main.py
+++ main.py
@@ -13,5 +13,10 @@ def index():
     return render_template("index.html", content=data)
 
 
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
